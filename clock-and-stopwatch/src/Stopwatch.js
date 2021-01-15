3a5e7b282480582bec4b3cb5d39cb6fed37e75c7
Maja <majacvetkovic@outlook.com>
2021-01-15 18:20:43 +0100

    🚀

--- clock-and-stopwatch/src/Stopwatch.js
+++ clock-and-stopwatch/src/Stopwatch.js
@@ -1,22 +1,47 @@
-import React, { useState, useEffect } from "../node_modules/react";
+import React, { useState, useEffect } from "react";
 
 export default function Stopwatch() {
-  const [date, setDate] = useState(new Date());
+  const [time, setTime] = useState({ h: 0, m: 0, s: 0, ms: 0 });
+  const [running, setRunning] = useState(false);
+
+  let updatedH = time.h,
+    updatedM = time.m,
+    updatedS = time.s,
+    updatedMs = time.ms;
+
+  function start() {
+    if (time.ms < 100) {
+      time.ms++;
+    } else if (time.ms === 100) {
+      updatedS++;
+      time.ms = 0;
+    }
+    if (time.s === 60) {
+      updatedM++;
+      time.s = 0;
+    }
+    if (time.m === 60) {
+      updatedH++;
+      time.m = 0;
+    }
+    return setTime({ h: updatedH, m: updatedM, s: updatedS, ms: updatedMs });
+  }
+
+  function run() {
+    setInterval(() => start(), 100);
+  }
 
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      setDate(new Date());
-    }, 1000);
-
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, [date]);
+    let id = setInterval(() => run(), 1000);
+    return () => clearInterval(id);
+  }, [time]);
 
   return (
     <>
-      <h2>{date.toLocaleTimeString()}</h2>
-      <button>Start/Pause</button>
+      <h2>
+        {time.h} : {time.m} :{time.s} :{time.ms}
+      </h2>
+      <button onClick={() => setRunning(!running)}>Start/Pause</button>
       <button>Stop</button>
     </>
   );
